b165e08bd4a37928922e51f175f3bf1ac70470ae
Abhishek Nipun <40643632+abhisheknipun1@users.noreply.github.com>
2019-07-01 14:38:50 +0530

    Powershell script from checking cookbook version

--- cookbook_version.ps1
+++ cookbook_version.ps1
@@ -1,7 +1,14 @@
 cls
+
 $output = Get-ChildItem "C:\cookbooks\create-new-file\metadata.rb"  | % { select-string $_ -pattern '^version' }
+
+# Give your path where metadata.rb file is present
+
 $output = $output.ToString()
-$len = $output.Length
-$len = $len - 1
-$cookbok_version = $output.Substring(52,$len-52)
+$output = $output.Split()
+$output = $output[1]
+
+$len = $output.Length -1
+
+$cookbok_version = $output.Substring(1,$len-1)
 Write-Host $cookbok_version
